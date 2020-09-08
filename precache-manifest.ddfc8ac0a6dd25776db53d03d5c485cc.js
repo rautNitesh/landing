cab50f53b0322f3a27f2b6b7ffe4c0af3fd7b624
Nitesh Raut <rautesh6@gmail.com>
2020-09-08 08:36:11 +0545

    Updates

---
+++ precache-manifest.ddfc8ac0a6dd25776db53d03d5c485cc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8878b4ca89ee814e7c9d28d38c112e71",
+    "url": "/landing/index.html"
+  },
+  {
+    "revision": "7f100537a6b4ab07d854",
+    "url": "/landing/static/css/main.4cdacf19.chunk.css"
+  },
+  {
+    "revision": "6b827bb5cdb0c72b8231",
+    "url": "/landing/static/js/2.89b50ca2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/landing/static/js/2.89b50ca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f100537a6b4ab07d854",
+    "url": "/landing/static/js/main.912287bd.chunk.js"
+  },
+  {
+    "revision": "c7b2fc2db9c09d6585ab",
+    "url": "/landing/static/js/runtime-main.f0c0ae33.js"
+  },
+  {
+    "revision": "d15e7e16d719e1867ec87294a3bc021d",
+    "url": "/landing/static/media/customize.d15e7e16.png"
+  },
+  {
+    "revision": "f55160c27014e3c4f9232c1d52111ce0",
+    "url": "/landing/static/media/delivery.f55160c2.png"
+  },
+  {
+    "revision": "2d6614cf923d7c24c9771167ce0ebf39",
+    "url": "/landing/static/media/fresh.2d6614cf.png"
+  },
+  {
+    "revision": "03d82f01b1fcf1de4ccb56b4430f1404",
+    "url": "/landing/static/media/smallPlate.03d82f01.png"
+  },
+  {
+    "revision": "023771c37e73d6d1a04d5da5e029aef4",
+    "url": "/landing/static/media/steak.023771c3.png"
+  },
+  {
+    "revision": "554be7328fdafd4a65c515e33619deff",
+    "url": "/landing/static/media/whitePlate.554be732.png"
+  }
+]);
